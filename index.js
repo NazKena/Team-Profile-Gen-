2d170939bfc83e61f143b0c8e4756a32907e290b
Nuseyba <n.kenadiid@gmail.com>
2023-02-28 18:51:56 +0000

    completed manager questions

--- index.js
+++ index.js
@@ -5,3 +5,44 @@ const Manager = require("./lib/Manager")
 const Engineer = require("./lib/Engineer")
 const Intern = require("./lib/Intern")
 
+const teamArray = [];
+
+function addManager () {
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            name:"managerName",
+            message: "Enter your Manager's name here",
+        },
+
+        {
+            type: "input",
+            name:"managerID",
+            message: "Enter your Manager's ID here",
+        },
+
+        {
+            type: "input",
+            name:"managerEmail",
+            message: "Enter your Manager's Email here",
+        },
+
+        {
+            type: "input",
+            name:"managerPhone",
+            message: "Enter your Manager's phone here",
+        },
+
+    ])
+
+    .then ((answers) => {
+        const manager = new Manager(
+            answers.managerName,
+            answers.managerID,
+            answers.managerEmail,
+            answers.managerPhone,
+        );
+    }
+    )
+}
